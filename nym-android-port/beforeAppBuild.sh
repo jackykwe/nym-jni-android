c424b656e1042622a2e3380959482e53d5f727bb
jackykwe <40329372+jackykwe@users.noreply.github.com>
2022-11-14 23:03:35 +0000

    Integrated .sh into gradle build process

--- nym-android-port/beforeAppBuild.sh
+++ nym-android-port/beforeAppBuild.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# Add executing this file to Android Studio build configuration under "external tools"
 
+currentDir=$(pwd)
+echo "$currentDir"
 mkdir -p app/src/main/jniLibs/x86_64
 
-cd ../NymAndroidPortRust1 || exit
-cargo build --color always
-cp target/x86_64-linux-android/debug/librustcode.so ../NymAndroidPort/app/src/main/jniLibs/x86_64
+cd ../NymAndroidPortRust1 || exit 1
+cargo build --color always --target aarch64-linux-android
+cargo build --color always --target x86_64-linux-android
+cd "$currentDir" || exit 1
+cp ../NymAndroidPortRust1/target/aarch64-linux-android/debug/librustcode.so app/src/main/jniLibs/aarch64
+cp ../NymAndroidPortRust1/target/x86_64-linux-android/debug/librustcode.so app/src/main/jniLibs/x86_64
 
-cd ../NymAndroidPortRust2 || exit
-cargo build --color always
-cp target/x86_64-linux-android/debug/librustcode2.so ../NymAndroidPort/app/src/main/jniLibs/x86_64
+cd ../NymAndroidPortRust2 || exit 1
+cargo build --color always --target aarch64-linux-android
+cargo build --color always --target x86_64-linux-android
+cd "$currentDir" || exit 1
+cp ../NymAndroidPortRust2/target/aarch64-linux-android/debug/librustcode2.so app/src/main/jniLibs/aarch64
+cp ../NymAndroidPortRust2/target/x86_64-linux-android/debug/librustcode2.so app/src/main/jniLibs/x86_64
 
 echo "OK"
