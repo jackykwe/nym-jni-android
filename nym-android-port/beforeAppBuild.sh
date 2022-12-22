f8e6f9a0c5cebcbe62ef01b4a52f11edb25ae0a6
jackykwe <40329372+jackykwe@users.noreply.github.com>
2022-12-22 03:17:32 +0000

    Updated bash script to only copy if .so file updated

--- nym-android-port/beforeAppBuild.sh
+++ nym-android-port/beforeAppBuild.sh
@@ -11,7 +11,7 @@ cd ../nym-jni || exit 2
 cargo build --color always --target aarch64-linux-android || exit 3
 #cargo build --color always --target x86_64-linux-android || exit 4
 cd "$currentDir" || exit 5
-cp ../nym-jni/target/aarch64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/arm64-v8a || exit 6
-#cp ../nym-jni/target/x86_64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/x86_64 || exit 7
+cp -u ../nym-jni/target/aarch64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/arm64-v8a || exit 6
+#cp -u ../nym-jni/target/x86_64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/x86_64 || exit 7
 
 echo "beforeAppBuild: OK"
