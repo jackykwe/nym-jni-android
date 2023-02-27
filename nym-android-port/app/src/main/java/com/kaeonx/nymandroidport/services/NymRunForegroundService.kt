96410209244206a72cb22e5724eee037407d5d74
jackykwe <40329372+jackykwe@users.noreply.github.com>
2023-02-27 23:13:15 +0000

    probe-effect-evaluation branch critical fix In the probe-effect-evaluation side of things, the Nym websocket that awaits messages from the client (i.e. Android app) doesn't expect any log_message_id to be prepended to the JSON message.

--- nym-android-port/app/src/main/java/com/kaeonx/nymandroidport/services/NymRunForegroundService.kt
+++ nym-android-port/app/src/main/java/com/kaeonx/nymandroidport/services/NymRunForegroundService.kt
@@ -203,9 +203,7 @@ class NymRunForegroundService : Service() {
                             val successfullyEnqueued =
                                 nymWebSocketClient.sendMessageThroughWebSocket(
                                     messageLogId = it.message,
-                                    message = "${it.message}${
-                                        NymMessageToSend.from(it).encodeToString()
-                                    }"
+                                    message = NymMessageToSend.from(it).encodeToString()
                                 )
                             if (successfullyEnqueued) {
                                 // prepare to send next pending-send message
