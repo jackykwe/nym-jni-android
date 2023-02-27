2a2436672742658021f0e773834d87322ba78f1d
jackykwe <40329372+jackykwe@users.noreply.github.com>
2023-02-27 23:06:46 +0000

    Added script to automatically collect data from physical device

--- nym-android-port/beforeAppBuild.sh
+++ nym-android-port/beforeAppBuild.sh
@@ -8,12 +8,28 @@ mkdir -p app/src/main/jniLibs/arm64-v8a
 mkdir -p app/src/main/jniLibs/x86_64
 
 cd ../nym-jni || exit 2
-cargo build --color always --target aarch64-linux-android --release || exit 3
-#cargo build --color always --target aarch64-linux-android || exit 3
+
+# IMPORTANT
+
+# 3 checks for DEBUG BUILD:
+# - Android Studio build variant is debug?
+# - --release flag is not used?
+# - Copying from .../debug/... folder instead of .../release/...?
+
+# 3 checks for RELEASE BUILD:
+# - Android Studio build variant is release?
+# - --release flag is used?
+# - Copying from .../release/... folder instead of .../debug/...?
+
+
+#cargo build --color always --target aarch64-linux-android --release || exit 3
+cargo build --color always --target aarch64-linux-android || exit 3
 #cargo build --color always --target x86_64-linux-android --release || exit 4
 #cargo build --color always --target x86_64-linux-android || exit 4
 cd "$currentDir" || exit 5
+#cp -u ../nym-jni/target/aarch64-linux-android/release/libnym_jni.so app/src/main/jniLibs/arm64-v8a || exit 6
 cp -u ../nym-jni/target/aarch64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/arm64-v8a || exit 6
+#cp -u ../nym-jni/target/x86_64-linux-android/release/libnym_jni.so app/src/main/jniLibs/x86_64 || exit 7
 #cp -u ../nym-jni/target/x86_64-linux-android/debug/libnym_jni.so app/src/main/jniLibs/x86_64 || exit 7
 
 echo "beforeAppBuild: OK"
